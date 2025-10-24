e29cb188907b8495e5405b251bff021c349ea578
_MangoLuv’sCode <140750964+PBJ-Labs@users.noreply.github.com>
2025-10-24 09:15:02 -0500

    Create grksl.py

---
+++ GRKSL/grksl.py
@@ -0,0 +1,37 @@
+#******************************
+# File : grksl.py
+# Praise The Lord, Jesus Christ
+#******************************
+
+##for placeholding
+grksltype = int
+##################
+
+GRKSL_LINE = 0x000
+GRKSL_PLANE = 0x001
+GRKSL_FACE = 0x002
+GRKSL_RECTANGLE = 0x003
+GRKSL_POLYGON = 0x004
+GRKSL_POLYHEDERON = 0x005
+GRKSL_BRUSH = 0x006
+GRKSL_CUBIOD = 0x007
+GRKSL_PRISM = 0x008
+GRKSL_PYRAMID = 0x009
+GRKSL_CYLINDER = 0x010
+GRKSL_SPHERE = 0x011
+GRKSL_CIRCLE = 0x012
+GRKSL_POINT = 0x013
+GRKSL_STIPPLE = 0x014
+GRKSL_XY = 0x015
+GRKSL_XZ = 0x016
+GRKSL_YZ = 0x017
+GRKSL_WINDOW = 0x018
+GRKSL_TEXT = 0x019
+GRKSL_BUFFER = 0x020
+GRKSL_COLOR = 0x021
+
+def grkslRender(grksltype):
+  any()
+
+def grkslEndRender(grksltype):
+  any()
